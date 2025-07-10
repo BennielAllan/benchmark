805a5924f54160b74562f25289574897354cc235
BennielAllan <2822046942@qq.com>
2025-07-10 19:12:42 +0800

    代理

---
+++ on_proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update
+apt install -y git
+apt install -y sed
+
+git clone https://gh-proxy.com/https://github.com/wanhebin/clash-for-linux.git
+
+cd clash-for-linux
+
+sed -i "s|^export CLASH_URL=.*$|export CLASH_URL='https://c8728336.jego.vip/subscribe/clash/1a4e6a2902d1522a7c552f695a24f49e'|" .env
+
+chmod +x start.sh
+./start.sh
