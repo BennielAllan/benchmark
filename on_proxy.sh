03f1dffeabc5e8b19d4dd19bcdf29a1700ab681c
BennielAllan <2822046942@qq.com>
2025-07-10 20:19:42 +0800

    更新代理地址

--- on_proxy.sh
+++ on_proxy.sh
@@ -7,7 +7,7 @@ git clone https://gh-proxy.com/https://github.com/wnlen/clash-for-linux.git
 
 cd clash-for-linux
 
-sed -i "s|^export CLASH_URL=.*$|export CLASH_URL='https://c8728336.jego.vip/subscribe/clash/1a4e6a2902d1522a7c552f695a24f49e'|" .env
+sed -i "s|^export CLASH_URL=.*$|export CLASH_URL='https://bfb70602.wyxhuawei.com/subscribe/clash/1a4e6a2902d1522a7c552f695a24f49e'|" .env
 
 chmod +x start.sh
 ./start.sh
